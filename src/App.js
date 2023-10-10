b48f4c041bf7e8dc2f377aa02637438e0d3e04d3
nareshbhalerao <root@ip-172-31-44-248.ap-south-1.compute.internal>
2023-10-10 09:15:34 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code></code> I was changed.
+           <code></code> I was changed on feature branch.
         </p>
         <a
           className="App-link"
